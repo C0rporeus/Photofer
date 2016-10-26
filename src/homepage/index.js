257943aebd123eb6da65015cc1421f516dccc7f3
F3nr1r <jhony2473@gmail.com>
2016-10-26 00:04:59 -0500

    creacion de modulos para el cuerpo, el encabezado y el pie de pagina del proyecto

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -1,6 +1,10 @@
 var page = require('page');
+var empty = require('empty-element');
+var template = require('./template');
+var title = require('title');
 
 page('/', function(ctx, next) {
+  title('Photofer');
   var main = document.getElementById('main-container');
-  main.innerHTML = '<a href="/signup">SignUp</a>';
-});
+  empty(main).appendChild(template); 
+})
