371a08fbe77d4bee481e80a720dfb7c9d446d52b
F3nr1r <jhony2473@gmail.com>
2017-01-26 00:57:07 -0500

    añadiendo internationalitation

--- src/picture-card/index.js
+++ src/picture-card/index.js
@@ -1,5 +1,18 @@
 var yo = require('yo-yo');
-var moment = require('moment');
+//var moment = require('moment');
+
+//el "sino" es por si el navegador no soporta rf, de esta forma cargaria una lbreria adicional de intl (pollyfill)
+if (!window.Intl) {
+  window.Intl = require('intl');
+  require('intl/locale-data/jsonp/en-US.js');
+  require('intl/locale-data/jsonp/es.js');
+}
+window.IntlRelativeFormat = require('intl-relativeformat');
+
+require('intl-relativeformat/dist/locale-data/en.js');
+require('intl-relativeformat/dist/locale-data/es.js')
+
+var rf = new IntlRelativeFormat('es');
 
 module.exports = function pictureCard(pic) {
 
@@ -15,7 +28,7 @@ module.exports = function pictureCard(pic) {
                               <img src="${pic.user.avatar}" class="avatar" />
                             <span class="username">${pic.user.username}</span>
                             </a>
-                            <small class="right time">${moment(picture.createdAt).fromNow()}</small>
+                            <small class="right time">${rf.format(picture.createdAt)}</small>
                             <p>
                             <a class="left" href="#" onclick=${like.bind(null, true)}><i class="fa fa-heart-o" aria-hidden="true"></i></a>
                             <a class="left" href="#" onclick=${like.bind(null, false)}><i class="fa fa-heart" aria-hidden="true"></i></a>
