c1b29f5de35991686bbc375db5f9eea0301e8824
F3nr1r <jhony2473@gmail.com>
2017-01-26 00:29:34 -0500

    añadiendo contadores y tiempo con idiomas

--- src/picture-card/index.js
+++ src/picture-card/index.js
@@ -1,20 +1,36 @@
 var yo = require('yo-yo');
+var moment = require('moment');
 
-module.exports = function (pic) {
-              return yo`<div class="card">
-                        <div class="card-image">
-                          <img class="activator" src="${pic.url}">
-                        </div>
-                        <div class="card-content">
-                          <a href="/user/${pic.user.username}" class="card-title">
-                            <img src="${pic.user.avatar}" class="avatar" />
-                          <span class="username">${pic.user.username}</span>
-                        </a>
-                        <small class="right time">Hace 1 día</small>
-                        <p>
-                          <a class="left" href="#"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
-                          <span class="left likes">${pic.likes} me gusta </span>
-                        </p>
-                        </div>
-                      </div>`;
+module.exports = function pictureCard(pic) {
+
+  var el;
+
+  function render(picture) { 
+                      return yo`<div class="card ${picture.liked ? 'liked' : ''}">
+                          <div class="card-image">
+                            <img class="activator" src="${pic.url}">
+                          </div>
+                          <div class="card-content">
+                            <a href="/user/${pic.user.username}" class="card-title">
+                              <img src="${pic.user.avatar}" class="avatar" />
+                            <span class="username">${pic.user.username}</span>
+                            </a>
+                            <small class="right time">${moment(picture.createdAt).fromNow()}</small>
+                            <p>
+                            <a class="left" href="#" onclick=${like.bind(null, true)}><i class="fa fa-heart-o" aria-hidden="true"></i></a>
+                            <a class="left" href="#" onclick=${like.bind(null, false)}><i class="fa fa-heart" aria-hidden="true"></i></a>
+                            <span class="left likes">${pic.likes} me gusta </span>
+                            </p>
+                          </div>
+                        </div>`;
+}
+function like(liked){
+    pic.liked = liked;
+    pic.likes += liked ? 1 : -1;
+    var newEL = render(pic);
+    yo.update(el, newEL);
+    return false;
+  }
+  el = render(pic);
+  return el;
 }
